21dbbffb29b3b36ebd29c618f87ecb9f7b6fd435
JaysonCheng <jinsengcheng@gmail.com>
2020-05-09 06:08:08 +0000

    initial deploy commit

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
+var flash = require("connect-flash");
+
+var User = require("./models/user");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+var seedDB = require("./seeds");
+
+// Requiring routes
+var commentRoutes = require("./routes/comments");
+var campgroundRoutes = require("./routes/campgrounds");
+var indexRoutes = require("./routes/index");
+
+// APP CONFIG
+// DATABASE CONFIG
+mongoose.connect("mongodb://localhost/yelp_camp", {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true
+}).then(() => {
+	console.log("Connected to MongoDB!");
+}).catch(err => {
+	console.log("ERRPR", err.message);
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// Seed the Database
+// seedDB();
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+	secret: "I love corgis",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+    console.log("YELP CAMP IS BOOTING UP!");
+});
---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+
+// Root Route
+router.get("/", function(req, res){
+   res.render("landing") ;
+});
+
+// AUTHENTICATION ROUTES
+// show register form
+router.get("/register", function(req, res){
+	res.render("register");
+});
+
+// Sign up logic
+router.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			req.flash("error", err.message);
+			return res.redirect("/register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			req.flash("success", "Welcome to YelpCamp, " + user.username) + "!";
+			res.redirect("/campgrounds");
+		});
+	});
+});
+
+// Show login form
+router.get("/login", function(req, res){
+	res.render("login");
+});
+
+// Handling login logic
+router.post("/login", passport.authenticate("local", 
+	{
+		successRedirect: "/campgrounds",
+		failureRedirect: "/login"
+	}), function(req, res){
+});
+
+// Logout route
+router.get("/logout", function(req, res){
+	req.logout();
+	req.flash("success", "You have successfully logged out");
+	res.redirect("/campgrounds");
+});
+
+module.exports = router;
